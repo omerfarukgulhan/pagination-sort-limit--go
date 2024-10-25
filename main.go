4f40240af7dcc05d253efa59ae972881ac4e5b21
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-25 13:52:50 +0300

    router in main file updated

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"pagination/common/app"
 	"pagination/common/postgresql"
+	"pagination/controller"
+	"pagination/persistence"
+	"pagination/service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +16,13 @@ func main() {
 	db := postgresql.GetConnection(configurationManager.PostgreSqlConfig)
 	postgresql.MigrateTables(db)
 
+	productRepository := persistence.NewProductRepository(db)
+	productService := service.NewProductService(productRepository)
+	productController := controller.NewProductController(productService)
+
 	router := gin.Default()
+
+	productController.RegisterProductRoutes(router)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
