9fdff7590abd13e7dcccab2c2c863ab4b9b63ede
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-10-27 13:16:11 +0300

    filter logic added

--- common/util/queryutils/filter.go
+++ common/util/queryutils/filter.go
@@ -1,7 +1,9 @@
 package queryutils
 
 import (
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type Filter struct {
@@ -14,27 +16,52 @@ func ApplyFilter(filters []Filter) func(*gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		for _, filter := range filters {
 			switch filter.Op {
-			case "=":
+			case "=", "eq":
 				db = db.Where(filter.Field+" = ?", filter.Value)
-			case ">":
+			case ">", "gt":
 				db = db.Where(filter.Field+" > ?", filter.Value)
-			case "<":
+			case "<", "lt":
 				db = db.Where(filter.Field+" < ?", filter.Value)
-			case ">=":
+			case ">=", "gte":
 				db = db.Where(filter.Field+" >= ?", filter.Value)
-			case "<=":
+			case "<=", "lte":
 				db = db.Where(filter.Field+" <= ?", filter.Value)
-			case "!=":
+			case "!=", "ne":
 				db = db.Where(filter.Field+" != ?", filter.Value)
-			case "LIKE":
-				db = db.Where(filter.Field+" LIKE ?", filter.Value)
-			case "IN":
-				db = db.Where(filter.Field+" IN ?", filter.Value)
-			case "NOT IN":
-				db = db.Where(filter.Field+" NOT IN ?", filter.Value)
-			default:
+			case "LIKE", "like":
+				db = db.Where(filter.Field+" LIKE ?", "%"+filter.Value.(string)+"%")
+			case "IN", "in":
+				values := strings.Split(filter.Value.(string), ",")
+				db = db.Where(filter.Field+" IN ?", values)
+			case "NOT IN", "not_in":
+				values := strings.Split(filter.Value.(string), ",")
+				db = db.Where(filter.Field+" NOT IN ?", values)
 			}
 		}
 		return db
 	}
+}
+
+func ParseFilters(c *gin.Context) []Filter {
+	filterQuery := c.Request.URL.Query()
+	var filters []Filter
+	for key, values := range filterQuery {
+		if !strings.HasPrefix(key, "filter[") {
+			continue
+		}
+		keyParts := strings.Split(key, "[")
+		if len(keyParts) < 3 {
+			continue
+		}
+		field := strings.TrimSuffix(keyParts[1], "]")
+		op := strings.TrimSuffix(keyParts[2], "]")
+		if len(values) > 0 {
+			filters = append(filters, Filter{
+				Field: field,
+				Value: values[0],
+				Op:    op,
+			})
+		}
+	}
+	return filters
 }
